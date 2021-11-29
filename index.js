49e629e94090b7191ff89ba1869108f9f42b2ef5
ifiyenia-ea <89512351+ifiyenia-ea@users.noreply.github.com>
2021-11-29 11:52:29 +0100

    assignment updated / finished

--- index.js
+++ index.js
@@ -1,4 +1,80 @@
 
+// SECOND TRY STARTS HERE :-)
+
+// first code to calculate - then add values of triangles ...
+// and then an if / else? if xx > yy, what will the message be - and how will I make it
+// work for all three calculations? am i meant to?
+
+// true - false ...
+
+//const area = (`${object}`.base * `{$object}`.height) / 2;
+
+
+//CASE 1
+const triangleOne = (10 * 13) / 2;
+const triangleTwo = (16.5 * 20.3) / 2;
+
+if (triangleOne > triangleTwo) {
+    console.log(`TriangleOne has an area of ${triangleOne} cm2, 
+    TriangleTwo has an area of ${triangleTwo} cm2.
+    The triangle with the biggest area is TriangleOne.`)
+} else if (triangleTwo > triangleOne) {
+    console.log(`TriangleOne has an area of ${triangleOne} cm2, 
+    TriangleTwo has an area of ${triangleTwo} cm2.
+    The triangle with the biggest area is TriangleTwo.`)
+} else if (triangleOne === triangleTwo) {
+    console.log(`TriangleOne has an area of ${triangleOne} cm2, 
+    TriangleTwo has an area of ${triangleTwo} cm2.
+    The triangles are the same size.`)
+}
+
+// so, I first had just the if + else, before remembering that I also need to have a third
+// option because of the second case. But since I'm not supposed to know the answers,
+// that third option should be on all three, of course - also learned that if "else if" says
+// in one place, it needs to be on "all elses"; cant be if xx else xx else if.
+
+//CASE 2
+const triangleThree = (16.5 * 20.3) / 2;
+const triangleFour = (20.3 * 16.5) / 2;
+
+if (triangleThree > triangleFour) {
+    console.log(`TriangleThree has an area of ${triangleThree} cm2, 
+    TriangleFour has an area of ${triangleFour} cm2.
+    The triangle with the biggest area is TriangleThree.`)
+} else if (triangleFour > triangleThree) {
+    console.log(`TriangleThree has an area of ${triangleThree} cm2, 
+    TriangleFour has an area of ${triangleFour} cm2.
+    The triangle with the biggest area is TriangleFour.`)
+} else if (triangleThree === triangleFour) {
+    console.log(`TriangleThree has an area of ${triangleThree} cm2,
+    TriangleFour has an area of ${triangleFour} cm2.
+    The two triangles are the same size.`)
+}
+
+
+//CASE 3
+const triangleFive = (7.8 * 5.6) / 2;
+const triangleSix = (9.3 * 12.4) / 2;
+
+if (triangleFive > triangleSix) {
+    console.log(`TriangleFive has an area of ${triangleFive} cm2, 
+    TriangleSix has an area of ${triangleSix} cm2.
+    The triangle with the biggest area is TriangleFive.`)
+} else if (triangleSix > triangleFive) {
+    console.log(`TriangleFive has an area of ${triangleFive} cm2, 
+    TriangleSix has an area of ${triangleSix} cm2.
+    The triangle with the biggest area is TriangleSix.`)
+} else if (triangleFive === triangleSix) {
+    console.log(`TriangleFive has an area of ${triangleFive} cm2,
+    TriangleSix has an area of ${triangleSix} cm2.
+    The two triangles are the same size.`)
+}
+
+
+
+
+
+
 // assignment: 
 // three cases, two triangles each. all in the same file?
 // template literals
@@ -7,7 +83,8 @@
 // it needs to prioritise the base * height.
 // how do I make a calculation of this? 
 
-console.log( (10 * 13) / 2)
+//console.log( (10 * 13) / 2)
+
 // working, yay!
 
 //const triangleOne = base = 10, height = 30;
@@ -28,22 +105,22 @@ console.log( (10 * 13) / 2)
 
 // CASE 1
 
-const triangleOne = {
-    base: 10,
-    height: 13
-};
+//const triangleOne = {
+  //  base: 10,
+    //height: 13
+//};
 
-let area = (triangleOne.base * triangleOne.height) / 2;
-console.log(area);
+//let area = (triangleOne.base * triangleOne.height) / 2;
+//console.log(area);
 // area = 65, correct!
 
-const triangleTwo = {
-    base: 16.5,
-    height: 20.3
-};
+//const triangleTwo = {
+ //   base: 16.5,
+  //  height: 20.3
+//};
 
-area = (triangleTwo.base * triangleTwo.height) / 2;
-console.log(area);
+//area = (triangleTwo.base * triangleTwo.height) / 2;
+//console.log(area);
 // area = 167,475, correct again!
 
 // RESULTS CASE 1: 
@@ -54,22 +131,22 @@ console.log(area);
 
 // CASE 2:
 
-const triangleThree = {
-    base: 16.5,
-    height: 20.3
-};
+//const triangleThree = {
+  //  base: 16.5,
+  //  height: 20.3
+//};
 
-area = (triangleThree.base * triangleThree.height) / 2;
-console.log(area);
+//area = (triangleThree.base * triangleThree.height) / 2;
+//console.log(area);
 // area = 167,75, again - correct
 
-const triangleFour = {
-    base: 20.3,
-    height: 16.5
-};
+//const triangleFour = {
+ //   base: 20.3,
+ //   height: 16.5
+//};
 
-area = (triangleFour.base * triangleFour.height) / 2;
-console.log(area);
+//area = (triangleFour.base * triangleFour.height) / 2;
+//console.log(area);
 // area = 167,75, correct
 
 // RESULTS CASE 2:
@@ -80,22 +157,22 @@ console.log(area);
 
 // CASE 3:
 
-const triangleFive = {
-    base: 7.8,
-    height: 5.6
-};
+//const triangleFive = {
+   // base: 7.8,
+   // height: 5.6
+//};
 
-area = (triangleFive.base * triangleFive.height) / 2;
-console.log(area)
+//area = (triangleFive.base * triangleFive.height) / 2;
+//console.log(area)
 // area = 21,84, correct
 
-const triangleSix = {
-    base: 9.3,
-    height: 12.4
-};
+//const triangleSix = {
+//    base: 9.3,
+//    height: 12.4
+//};
 
-area = (triangleSix.base * triangleSix.height) / 2;
-console.log(area);
+//area = (triangleSix.base * triangleSix.height) / 2;
+//console.log(area);
 // area = 57,66(0000000000...4), correct!
 
 // RESULTS CASE 3:
